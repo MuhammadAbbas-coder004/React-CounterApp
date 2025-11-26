6953269f408f1de644fa4edbbf5f7cb40a2a50b4
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2025-11-26 21:38:00 +0500

    counter app completed

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+let [num, setNum] = useState(0);
+const addnum = ()=>{
+setNum(num + 1);
+
+}
+
+const subnum = ()=>{
+if(num === 0){
+console.log("No subtriction");
+
+}else{
+  setNum(num - 1);
+}
+
+}
+  return (
+    <>
+    <div id="app-container">
+      <h1 id="app-heading">Counter App</h1>
+
+      <div id="counter-display">{num}</div>
+
+      <div id="button-container">
+        <button id="decrement-btn" onClick={subnum} >
+          − Decrement
+        </button>
+        <button id="increment-btn"  onClick={addnum}>
+          + Increment
+        </button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
